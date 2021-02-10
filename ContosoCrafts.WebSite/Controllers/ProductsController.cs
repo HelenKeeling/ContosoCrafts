0af9f8ad99524d32296c28ca99bbd7bd5c6da9d2
Helen Keeling <helenlkeeling@gmail.com>
2021-02-10 19:53:21 +0000

    finished tutorial for dotnet

--- ContosoCrafts.WebSite/Controllers/ProductsController.cs
+++ ContosoCrafts.WebSite/Controllers/ProductsController.cs
@@ -24,6 +24,17 @@ namespace ContosoCrafts.WebSite.Controllers
         {
             return ProductService.GetProducts();
         }
+        //[HttpPatch] "[FromBody]"
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get (
+            [FromQuery] string ProductId,
+            [FromQuery] int Rating)
 
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+
+        }
     }
 }
